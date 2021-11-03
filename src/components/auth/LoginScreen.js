b01a8e379bc1163141f94679b12a69819a32ddb9
Fran2333 <00161218@uca.edu.sv>
2021-11-03 11:22:33 -0600

    fix: css login y agregando

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,4 +1,21 @@
 import React from 'react';
+import Typed from 'react-typed';
+import './LoginScreen.css';
+import logo from '../../assets/images/large_blanco2.png'
+
+const textLines = [
+    `Marca`,
+    `Restaurante`,
+    `Marca de ropa`,
+    `Pastelería`,
+     `Beauty shop`,
+     `Coffe shop`,
+     `Tienda de zapatos`,
+     `Estudio de arquitectura`,
+     `Pop up shop`,
+     `Paletería`,
+     `Tienda de accesorios`
+];
 
 export const LoginScreen = () => {
     return (
@@ -7,16 +24,19 @@ export const LoginScreen = () => {
                 <div className="row clearfix">
                     <div className="col-12">
                         <nav className="navbar navbar-expand-lg">
-                            <a className="navbar-brand text-monospace2" href="/">hola texto</a>
+                            <img src={logo} className="logo"/>
                         </nav>
                     </div>
                     <div className="col-lg-8">
                         <div className="auth_detail2">
                             <h2>Potenciamos<br></br>El éxito de tu</h2>
+                            <div>
+                                <Typed strings={textLines} typeSpeed={100} backSpeed={60} className="typed"/>
+                            </div>
                             <ul className="social-links list-unstyled">
-                            <li><a className="btn btn-default" href="https://www.facebook.com/arteriaestudio.sv" data-toggle="tooltip" data-placement="top" title="facebook"><i className="fa fa-facebook"></i></a></li>
-                            <li><a className="btn btn-default" href="https://www.behance.net/arteriaestudiosv" data-toggle="tooltip" data-placement="top" title="behance"><i className="fa fa-behance"></i></a></li>
-                            <li><a className="btn btn-default" href="https://www.instagram.com/arteriaestudio.sv/?hl=es-la" data-toggle="tooltip" data-placement="top" title="instagram"><i className="fa fa-instagram"></i></a></li>
+                            <li><a className="btn btn-default" href="https://www.facebook.com/arteriaestudio.sv" data-toggle="tooltip" data-placement="top" title="facebook"><i className="fab fa-facebook"></i></a></li>
+                            <li><a className="btn btn-default" href="https://www.behance.net/arteriaestudiosv" data-toggle="tooltip" data-placement="top" title="behance"><i className="fab fa-behance"></i></a></li>
+                            <li><a className="btn btn-default" href="https://www.instagram.com/arteriaestudio.sv/?hl=es-la" data-toggle="tooltip" data-placement="top" title="instagram"><i className="fab fa-instagram"></i></a></li>
                             <li><a className="btn btn-default" href="https://www.arteriaestudio.com/" data-toggle="tooltip" data-placement="top" title="web"><i className="fa fa-globe"></i></a></li>
                             </ul>
                         </div>
